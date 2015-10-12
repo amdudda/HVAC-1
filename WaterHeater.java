dc3844182b1e2bee1577b960fbf988c81459ff0d
amdudda <tk0654wm@go.minneapolis.edu>
2015-10-12 14:22:57 -0500

    Minor edit to tidy up display of WaterHeater.toString()

--- WaterHeater.java
+++ WaterHeater.java
@@ -36,7 +36,7 @@ public class WaterHeater extends ServiceCall {
                 "Resolved Date: " + resolvedDateString + "\n" +
                 "Resolution Description: " + resolutionString + "\n" +
                 "Base Fee: " + feeString + "\n" +
-                "City Fee: $" + this.city_fee + "\n" +
+                "City Fee: $" + this.city_fee + ".00\n" +
                 "Total Fees: " + totalFeeString + "\n";
     }  // end toString override
 
